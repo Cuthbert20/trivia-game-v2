851db2cf9ea715378ae50977f0593bcb63c16419
Cuthbert20 <knowltonspencer@gmail.com>
2019-12-03 11:25:08 -0700

    setup register and login components render on landing component

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+
+function Landing() {
+  return (
+    <div>
+      <h1>Landing Component</h1>
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default Landing;
