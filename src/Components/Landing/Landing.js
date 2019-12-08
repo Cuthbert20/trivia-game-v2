75d7e6d3ae62377263a701e6ca23eaca4945c754
Cuthbert20 <knowltonspencer@gmail.com>
2019-12-08 15:01:22 -0700

    adding font from google fonts, adding linear-gradient

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
+import "./Landing.css";
 
 function Landing() {
   return (
-    <div>
-      <h1>Landing Component</h1>
+    <div className="Landing-Container">
+      <nav>
+        <a href="!#">Home</a>
+      </nav>
+      <h1>Landing Component displays the following</h1>
       <Login />
       <Register />
+      <footer>Contact Us</footer>
     </div>
   );
 }
