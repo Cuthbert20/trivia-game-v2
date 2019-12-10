df9768f53353c4d721028345c665464f8ceff515
Cuthbert20 <knowltonspencer@gmail.com>
2019-12-10 10:11:00 -0700

    setting up store and reducer with Redux

---
+++ src/ducks/reducer.js
@@ -0,0 +1,21 @@
+//INITIAL STATE
+const initialState = {
+  userId: null,
+  email: "",
+  username: "",
+  profilePic: "",
+  firstName: "",
+  lastName: ""
+};
+
+//ACTION CONSTANTS
+
+//ACTION BUILDERS
+
+//REDUCER
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
