5fbb61ad5ec4e0c879853aa6fffc4365ffded19f
Cuthbert20 <knowltonspencer@gmail.com>
2019-12-12 12:54:27 -0700

    create material design style input for username

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import "./Login.css";
-
+// const floatField = document.getElementById("floatField");
+// const floatContainer = document.querySelector("#floatContainer");
+// floatField.addEventListener("focus", () => {
+//   floatContainer.classList.add("active");
+// });
+// floatContainer.addEventListener("blur", () => {
+//   floatContainer.classList.remove("active");
+// });
 function Login(props) {
+  const [isActive, setIsActive] = useState(false);
+  let handleFocus = () => {
+    setIsActive(!isActive);
+  };
+  console.log(isActive);
   return (
     <div className="Login-Container">
-      <article id="floatContainerUser" className="float-container">
-        <label for="floatField">Username</label>
-        <input input id="floatField" className="input" type="text" />
+      <article
+        id="floatContainerUser"
+        className={isActive ? "float-container active" : "float-container"}
+      >
+        <label htmlFor="floatField">Username</label>
+        <input
+          onFocus={handleFocus}
+          onBlur={handleFocus}
+          id="floatField"
+          className="input"
+          type="text"
+        />
       </article>
       <br />
       <article id="floatContainerPass" className="float-container">
